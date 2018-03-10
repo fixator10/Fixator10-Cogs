6c633a5bbf53d6f6385baad4ea61ecb02ec9b003
Fixator10 <fixator10@users.noreply.github.com>
2018-03-10 11:58:13 +0400

    [Minecraft Data] missing "/" in URL

--- minecraftdata/minecraftdata.py
+++ minecraftdata/minecraftdata.py
@@ -64,7 +64,7 @@ class MinecraftData:
             await self.bot.say(chat.error("This player not found"))
             return
         em = discord.Embed(timestamp=ctx.message.timestamp)
-        em.set_author(name=nickname, url="https:/capes.labymod.net/capes/{}".format(uuid))
+        em.set_author(name=nickname, url="https://capes.labymod.net/capes/{}".format(uuid))
         em.set_image(url="https://capes.labymod.net/capes/{}".format(uuid))
         await self.bot.say(embed=em)
 
