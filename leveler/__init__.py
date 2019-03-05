f783824e108ea8d8c1f6a188d5c8b1d3981353bf
Fixator10 <fixator10@users.noreply.github.com>
2019-03-05 13:51:01 +0400

    [datautils] add async initialize func call

--- leveler/__init__.py
+++ leveler/__init__.py
@@ -1,7 +1,8 @@
 from .leveler import Leveler
 
 
-def setup(bot):
+async def setup(bot):
     n = Leveler(bot)
+    await n.initialize()
     bot.add_listener(n._handle_on_message, "on_message")
     bot.add_cog(n)
