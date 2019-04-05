f0c1411c8650e482386756049437337bf28c75bb
Fixator10 <fixator10@users.noreply.github.com>
2019-04-05 15:17:54 +0400

    [minecraftdata] fix TypeError

--- minecraftdata/minecraftdata.py
+++ minecraftdata/minecraftdata.py
@@ -257,9 +257,9 @@ class MinecraftData(commands.Cog):
                 if k == key:
                     yield v
                 if isinstance(v, dict):
-                    for result in self.gen_dict_extract(key, v):
+                    async for result in self.gen_dict_extract(key, v):
                         yield result
                 elif isinstance(v, list):
                     for d in v:
-                        for result in self.gen_dict_extract(key, d):
+                        async for result in self.gen_dict_extract(key, d):
                             yield result
