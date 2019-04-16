08153371c32236647c4369c5a9474780d6d8cf06
Fixator10 <fixator10@users.noreply.github.com>
2019-04-16 16:59:29 +0400

    [minecraftdata] add i18n support

--- minecraftdata/converters.py
+++ minecraftdata/converters.py
@@ -3,6 +3,9 @@ from uuid import UUID
 from aiohttp import ContentTypeError
 from redbot.core.commands import BadArgument
 from redbot.core.commands import Converter
+from redbot.core.i18n import Translator
+
+_ = Translator("MinecraftData", __file__)
 
 
 class MCUUID:
@@ -17,15 +20,15 @@ class MCNickname(Converter):
         session = ctx.cog.session
         try:
             async with session.get(
-                "https://api.mojang.com/users/profiles/minecraft/" + argument
+                    f"https://api.mojang.com/users/profiles/minecraft/{argument}"
             ) as data:
                 response_data = await data.json()
         except ContentTypeError:
             response_data = None
         except Exception as e:
-            raise BadArgument(f"Unable to get data from Minecraft API: {e}")
+            raise BadArgument(_("Unable to get data from Minecraft API: {}").format(e))
         if response_data is None or "id" not in response_data:
-            raise BadArgument("{} not found on Mojang servers".format(argument))
+            raise BadArgument(_("{} not found on Mojang servers").format(argument))
         uuid = str(response_data["id"])
         name = str(response_data["name"])
         return MCUUID(name, uuid)
