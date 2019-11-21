05448deff22f4a6dc16541b8e5f581eed19931a9
Fixator10 <fixator10@users.noreply.github.com>
2019-11-21 11:29:01 +0400

    [weather] fix again

--- weather/weather.py
+++ weather/weather.py
@@ -494,7 +494,7 @@ class Weather(commands.Cog):
     async def get_units(self, ctx: commands.Context, units_type: str):
         """Get translated contextual units for type"""
         if not ctx.guild:
-            return UNITS.get(await self.config.user(ctx.author).units(), UNITS["si"])
+            return UNITS.get(await self.config.user(ctx.author).units(), UNITS["si"]).get(units_type, "?")
         current_system = (
                 await self.config.user(ctx.author).units()
                 or await self.config.guild(ctx.guild).units()
