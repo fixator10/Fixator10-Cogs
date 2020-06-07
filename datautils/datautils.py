f0adef31486756e75aca88bc10323a9123babca5
Fixator10 <fixator10@users.noreply.github.com>
2020-06-07 17:54:57 +0400

    [datautils] fix error if no permissions in channel
    attempt #2

--- datautils/datautils.py
+++ datautils/datautils.py
@@ -74,7 +74,7 @@ async def find_app_by_name(where: list, name: str):
 class DataUtils(commands.Cog):
     """Commands for getting information about users or servers."""
 
-    __version__ = "2.3.2"
+    __version__ = "2.3.3"
 
     # noinspection PyMissingConstructor
     def __init__(self, bot):
@@ -597,7 +597,7 @@ class DataUtils(commands.Cog):
             "{}\n{}".format(
                 chat.inline(str(perms.value)),
                 chat.box(
-                    chat.format_perms_list(perms) if perms else _("No permissions"), lang="py"
+                    chat.format_perms_list(perms) if perms.value else _("No permissions"), lang="py"
                 ),
             )
         )
