be661863bd0e261ffb996b7f22eaf3c9317d9f35
Fixator10 <fixator10@users.noreply.github.com>
2020-08-27 12:24:14 +0400

    another try

--- leveler/__init__.py
+++ leveler/__init__.py
@@ -2,17 +2,17 @@ from .leveler import Leveler
 
 __red_end_user_data_statement__ = (
     "This cog persistently stores next data about user:<br>"
-    "    Discord user ID<br>"
-    "    Current username<br>"
-    "    Per-server XP data (current XP and current level)<br>"
-    "    Total count of XP on all servers<br>"
-    "    URLs to profile/rank/levelup backrounds<br>"
-    "    User-set title & info<br>"
-    '    User\'s "reputation" points count<br>'
-    "    Data about user's badges<br>"
-    "    Data about user's profile/rank/levelup colors<br>"
-    "    Timestamp of latest message and reputation<br>"
-    "    Last message text (for comparison)<br>"
+    "&#32;• Discord user ID<br>"
+    "&#32;•  Current username<br>"
+    "&#32;•  Per-server XP data (current XP and current level)<br>"
+    "&#32;•  Total count of XP on all servers<br>"
+    "&#32;•  URLs to profile/rank/levelup backrounds<br>"
+    "&#32;•  User-set title & info<br>"
+    '&#32;•  User\'s "reputation" points count<br>'
+    "&#32;•  Data about user's badges<br>"
+    "&#32;•  Data about user's profile/rank/levelup colors<br>"
+    "&#32;•  Timestamp of latest message and reputation<br>"
+    "&#32;•  Last message text (for comparison)<br>"
     "This cog supports data removal requests."
 )
 
