8d92880e03314815cedeead1c845ef38dd0f509d
Jyu Viole Grace <24418520+thisisjvgrace@users.noreply.github.com>
2020-09-13 12:45:34 +0530

    forgot to bump version
    ty fixator <3

--- moreutils/moreutils.py
+++ moreutils/moreutils.py
@@ -43,7 +43,7 @@ def bool_emojify(bool_var: bool) -> str:
 class MoreUtils(commands.Cog):
     """Some (maybe) useful utils."""
 
-    __version__ = "2.0.6"
+    __version__ = "2.0.7"
 
     # noinspection PyMissingConstructor
     def __init__(self, bot):
