e0c558ebb14c9065458e8924b6ea7c5166e23e60
Jyu Viole Grace <24418520+thisisjvgrace@users.noreply.github.com>
2020-09-13 12:46:42 +0530

    [MoreUtils] round off CMYK, HSV, HSL, YIQ values (#48)

--- moreutils/moreutils.py
+++ moreutils/moreutils.py
@@ -43,7 +43,7 @@ def bool_emojify(bool_var: bool) -> str:
 class MoreUtils(commands.Cog):
     """Some (maybe) useful utils."""
 
-    __version__ = "2.0.6"
+    __version__ = "2.0.7"
 
     # noinspection PyMissingConstructor
     def __init__(self, bot):
@@ -87,10 +87,10 @@ class MoreUtils(commands.Cog):
                 color_name,
                 str(color),
                 colorrgb,
-                colorcmyk,
-                colorhsv,
-                colorhls,
-                coloryiq,
+                tuple(map(lambda x: isinstance(x, float) and round(x, 2) or x, colorcmyk)),
+                tuple(map(lambda x: isinstance(x, float) and round(x, 2) or x, colorhsv)),
+                tuple(map(lambda x: isinstance(x, float) and round(x, 2) or x, colorhls)),
+                tuple(map(lambda x: isinstance(x, float) and round(x, 2) or x, coloryiq)),
                 color.value,
             ),
             url=f"http://www.color-hex.com/color/{str(color)[1:]}",
