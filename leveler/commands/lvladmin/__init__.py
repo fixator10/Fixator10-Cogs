5c4e567e2630300549f7fcb764bf61a823b77567
Fixator10 <fixator10@users.noreply.github.com>
2020-09-20 17:30:58 +0400

    "debug" commands

--- leveler/commands/lvladmin/__init__.py
+++ leveler/commands/lvladmin/__init__.py
@@ -2,6 +2,7 @@ from leveler.abc import CompositeMetaClass
 
 from .backgrounds import Backgrounds
 from .badge import Badge
+from .debugging import Debugging
 from .economy import Economy
 from .roles import Roles
 from .settings import Settings
@@ -9,6 +10,6 @@ from .users import Users
 
 
 class LevelAdmin(
-    Backgrounds, Economy, Roles, Settings, Users, Badge, metaclass=CompositeMetaClass
+    Backgrounds, Economy, Roles, Settings, Users, Badge, Debugging, metaclass=CompositeMetaClass
 ):
     """Leveler administration commands"""
---
+++ leveler/commands/lvladmin/debugging.py
@@ -0,0 +1,58 @@
+from collections import Counter
+
+from redbot.core import commands
+from redbot.core.utils import chat_formatting as chat
+from tabulate import tabulate
+
+from leveler.abc import MixinMeta
+
+from .basecmd import LevelAdminBaseCMD
+
+
+class Debugging(MixinMeta):
+    """Badge administration commands"""
+
+    lvladmin = getattr(LevelAdminBaseCMD, "lvladmin")
+
+    @lvladmin.group(hidden=True, name="debug")
+    @commands.is_owner()
+    async def debug_commands(self, ctx):
+        """Debugging commands.
+
+        Dont use it until you know what you doing."""
+
+    @debug_commands.group(name="integrity")
+    async def db_integrity(self, ctx):
+        """Database integrity commands"""
+
+    @db_integrity.command(name="check")
+    async def db_integrity_check(self, ctx):
+        """Check Database integrity
+
+        Everything should be True. Otherwise there is malfunction somewhere in XP handling."""
+        c = Counter()
+        async for user in self.db.users.find({}):
+            total_xp = 0
+            for server in user["servers"]:
+                xp = await self._level_exp(user["servers"][server]["level"])
+                total_xp += xp
+                total_xp += user["servers"][server]["current_exp"]
+            c[total_xp == user["total_exp"]] += 1
+        await ctx.send(chat.box(tabulate(c.most_common())))
+
+    @db_integrity.command(name="fix")
+    async def db_integrity_fix(self, ctx):
+        """Artificially fix Database integrity
+
+        Everything should be True. Otherwise there is malfunction somewhere in XP handling."""
+        async for user in self.db.users.find({}):
+            total_xp = 0
+            for server in user["servers"]:
+                xp = await self._level_exp(user["servers"][server]["level"])
+                total_xp += xp
+                total_xp += user["servers"][server]["current_exp"]
+            if total_xp != user["total_exp"]:
+                await self.db.users.update_one(
+                    {"user_id": user["user_id"]}, {"$set": {"total_exp": total_xp}}
+                )
+        await ctx.tick()
