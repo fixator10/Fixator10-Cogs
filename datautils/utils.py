0498b1417ce85a8d390ea4bf69304ad9a7fd69fe
Fixator10 <github@fixator10.ru>
2021-04-18 22:40:29 +0400

    [datautils] use dpy menus instead of red's
    split in multiple files

---
+++ datautils/utils.py
@@ -0,0 +1,28 @@
+from redbot.core.i18n import Translator
+from redbot.core.utils import AsyncIter
+
+from .common_variables import TWEMOJI_URL
+
+_ = Translator("DataUtils", __file__)
+
+
+async def get_twemoji(emoji: str):
+    emoji_unicode = []
+    for char in emoji:
+        char = hex(ord(char))[2:]
+        emoji_unicode.append(char)
+    if "200d" not in emoji_unicode:
+        emoji_unicode = list(filter(lambda c: c != "fe0f", emoji_unicode))
+    emoji_unicode = "-".join(emoji_unicode)
+    return f"{TWEMOJI_URL}/{emoji_unicode}.png"
+
+
+def bool_emojify(bool_var: bool) -> str:
+    return "✅" if bool_var else "❌"
+
+
+async def find_app_by_name(where: list, name: str):
+    async for item in AsyncIter(where):
+        for k, v in item.items():
+            if v == name:
+                return item
