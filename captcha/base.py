c48a704bed0bc837c42993abb38c2f76709c4730
Fixator10 <github@fixator10.ru>
2022-07-02 10:13:22 +0400

    [captcha] fixed `was_loaded_once` config value

--- captcha/base.py
+++ captcha/base.py
@@ -23,7 +23,10 @@ from .events import Listeners
 from .informations import __author__, __patchnote__, __patchnote_version__, __version__
 from .utils import build_kick_embed
 
-DEFAULT_GLOBAL = {"log_level": 50}
+DEFAULT_GLOBAL = {
+    "log_level": 50,
+    "was_loaded_once": False,  # If this is the first time we load the cog. The value isn't really interesting
+}
 DEFAULT_GUILD = {
     "channel": None,  # The channel where the captcha is sent.
     "logschannel": None,  # Where logs are sent.
@@ -33,7 +36,6 @@ DEFAULT_GUILD = {
     "type": "plain",  # Captcha type.
     "timeout": 5,  # Time in minutes before kicking.
     "retry": 3,  # The number of retry allowed.
-    "was_loaded_once": False,  # If this is the first time we load the cog. The value isn't really interesting
 }
 log = logging.getLogger("red.fixator10-cogs.captcha")
 
@@ -378,7 +380,7 @@ class Captcha(
         if send_patchnote:
             await self._send_patchnote()
 
-        await self.config.was_loaded_once.set(True)
+        await self.data.was_loaded_once.set(True)
 
     async def _send_patchnote(self) -> None:
         await self.bot.wait_until_red_ready()
