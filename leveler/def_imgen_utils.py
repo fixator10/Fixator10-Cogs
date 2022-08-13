d31dc1e47c515337f821edac2c4efc3a3d6466a4
aikaterna <20862007+aikaterna@users.noreply.github.com>
2022-08-13 11:48:22 -0700

    getlength provides a float when we need int

--- leveler/def_imgen_utils.py
+++ leveler/def_imgen_utils.py
@@ -108,7 +108,7 @@ class DefaultImageGeneratorsUtils(MixinMeta):
     # finds the the pixel to center the text
     def _center(self, start, end, text, font):
         dist = end - start
-        width = font.getlength(text)[0]
+        width = int(font.getlength(text))
         start_pos = start + ((dist - width) / 2)
         return int(start_pos)
 
