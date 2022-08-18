a4d638e2ebd1c39959c9e654e44e09419172d46a
Fixator10 <github@fixator10.ru>
2022-08-18 21:33:07 +0400

    bump version

--- leveler/leveler.py
+++ leveler/leveler.py
@@ -29,7 +29,7 @@ class Leveler(
 ):
     """A level up thing with image generation!"""
 
-    __version__ = "3.0.5"
+    __version__ = "3.0.6"
 
     # noinspection PyMissingConstructor
     def __init__(self, bot: Red):
