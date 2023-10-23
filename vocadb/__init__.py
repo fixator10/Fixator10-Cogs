284520d1f85e5c1f80a16a114a345be4eff15de9
MAX <63972751+ltzmax@users.noreply.github.com>
2023-10-23 17:51:02 +0200

    and the rest.

--- vocadb/__init__.py
+++ vocadb/__init__.py
@@ -3,5 +3,5 @@ from .vocadb import VocaDB
 __red_end_user_data_statement__ = "This cog does not persistently store any PII data about users."
 
 
-def setup(bot):
-    bot.add_cog(VocaDB(bot))
+async def setup(bot):
+    await bot.add_cog(VocaDB(bot))
